43fddf2979a9e1587c2e66a46b47c2e547c7fd70
alexwastook <deudonalex@gmail.com>
2024-12-07 02:43:37 +0100

    livrable 1

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import argparse
+import argparse, json, sys
 
 parser = argparse.ArgumentParser()
 
@@ -7,16 +7,23 @@ parser.add_argument(
     help="Un fichier au format binaire contenant les instructions à décoder",
     type=str,
 )
-args = parser.parse_args()
 
-buffer_size = 4 # en 'bytes' (octets)
+args = parser.parse_args()
+buffer_size = 4  # en 'bytes' (octets)
+instructions = json.load(open("res/instructions.json"))
 
 offset = 0
+print("offset,valeur,opcode,encoding")
 with open(args.FICHIER_BIN, "rb") as f:
-    while (dword := f.read(buffer_size)): # walrus operator 🤩
+    while dword := f.read(buffer_size):  # walrus operator 🤩
+        hex_str = dword.hex().replace("0x", "")
+        opcode = bin(int(hex_str, 16))[2:].zfill(buffer_size * 8)[-7:]
+        instruction_type, encoding = instructions[opcode]
         print(
             str(offset).zfill(8),
-            dword.hex(),
-            sep=" ",
+            hex_str,
+            instruction_type,
+            encoding,
+            sep=",",
         )
         offset += 1
