2fd62813f98889f718ac1d6afe4cacf0b0ccf426
alexwastook <deudonalex@gmail.com>
2024-12-09 23:28:48 +0100

    begin decompile

--- main.py
+++ main.py
@@ -1,12 +1,41 @@
-from models import Instruction_I, Instruction_R, Instruction_S, Instruction_U
-import csv
-from string import ascii_lowercase, digits
+from models import Instruction
+import pandas as pd
+import argparse
 
-x = Instruction_I(ascii_lowercase + digits)
-print(x)
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "FICHIER_BIN",
+    help="Un fichier au format binaire contenant les instructions à décoder",
+    type=str,
+)
+args = parser.parse_args()
 
-# with open("res/instructions.csv", "r") as f:
-#     reader = csv.DictReader(f)
-#     headers = next(reader)
-#     for row in reader:
-#         print(row)
+df = pd.read_csv(
+    "res/instructions.csv",
+    dtype={i: str for i in ["opcode_value", "funct3", "funct7", "funct1"]},
+)
+buffer_size = 4  # en 'bytes' (octets)
+
+i_type_to_class = {c.__name__[0]: c for c in Instruction.__subclasses__()}
+
+offset = 0
+with open(args.FICHIER_BIN, "rb") as f:
+    while dword := f.read(buffer_size):  # walrus operator 🤩
+        hex_str = dword.hex()
+        bin_str = bin(int(hex_str, 16))[2:].zfill(32)
+
+        opcode = bin_str[-7:]
+
+        rows = df[df["opcode_value"] == opcode]
+        if rows.shape[0] == 1:
+            row = rows.iloc[0]
+        else:
+            funct3 = bin_str[12:15]
+            row = rows[rows["funct3"] == funct3].iloc[0]
+
+        i_type = row["type_encodage"][0]
+        print(
+            hex(offset * 4)[2:].zfill(8) + ":",
+            row["instruction"],
+            sep=" ",
+        )
